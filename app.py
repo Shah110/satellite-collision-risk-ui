8ab1f4020edee6f12f3612d70b98fbd7b9885491
Syed Razavi <syed.h.ra7avi@gmail.com>
2025-12-30 01:14:45 +0300

    Enhance UI with project notebook links and info update
    Updated sidebar with links to project notebooks and modified info message.

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
 st.set_page_config(page_title="Satellite Collision Risk UI", layout="centered")
-
 st.title("🛰️ Satellite Collision Risk UI")
 st.write("App is running ✅")
-st.info("Next: connect your trained model + preprocessing pipeline.")
+
+st.sidebar.header("📌 Project Notebooks (Colab)")
+st.sidebar.markdown("- [Phase 1](https://colab.research.google.com/drive/1Utaq_FtgsHhV215frPX-CQhC6nMwkfs3?usp=sharing)")
+st.sidebar.markdown("- [Phase 2](https://colab.research.google.com/drive/1oog4BFnxr5ohss8HHJszAzv2rZqWFJO3?usp=sharing)")
+st.sidebar.markdown("- [Phase 3](https://colab.research.google.com/drive/1DGncV8CkSyTKbe5YBfqrTC-qrQxwfau-?usp=sharing)")
+
+st.info("Next: connect your trained model + data source.")
